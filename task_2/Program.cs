b5982b4d45a0d311201b49e33e99598965195f36
Hmuriy Monakh <max_08931993@mail.ru>
2022-10-23 21:11:06 +0300

    task2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -5,10 +5,19 @@
 
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+Console.WriteLine("Введите значения b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значения b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значения k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значения k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
 
+double x = (b2 - b1) / (k2 - k1);
+double y = x * k1 + b1;
 
-
-
+Console.WriteLine($"точку пересечения x={x}, y={y} ");
 
 
 
